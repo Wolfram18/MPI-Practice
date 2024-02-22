293e77cf88e2f467d1fbf2cea71e219fe844f0cb
Victoria Ruleva <ruleva_18@mail.ru>
2024-02-22 14:29:54 +0300

    Update CalculationPi.cpp

--- src/CalculationPi.cpp
+++ src/CalculationPi.cpp
@@ -4,6 +4,7 @@
 #include <mpi.h>
 
 #define DEFAULT_N 100
+#define DEFAULT_METHOD 1
 #define PI 3.1415926535897932384626433832795
 
 void help()
@@ -12,51 +13,58 @@ void help()
     printf("\n using the definite integral method.");
     printf("\n\n The number of iterations (the \"N\" parameter)");
     printf("\n can be specified after the program name at startup.");
-    printf("\n By default the number of iterations is: %d.\n", DEFAULT_N);
+    printf("\n By default the number of iterations is: %d.", DEFAULT_N);
+    printf("\n\n The definite integral method (the \"method\" parameter)");
+    printf("\n can be specified after the program name at startup:");
+    printf("\n 1. Rectangle Method");
+    printf("\n 2. Trapezoidal Method");
+    printf("\n 3. Simpson Method");
+    printf("\n By default the method is: %d.\n", DEFAULT_METHOD);
 }
 
 double f(double x)
 {
-    return 1 / (1 + x * x);
+    return 1/(1+x*x);
 }
 
 double rectangle_method(int N, int world_size, int world_rank) 
 {
-    double h, sum = 0;
-    int a = 0, b = 1;
-    h = 1.0 / N;
-    for (int i = world_rank + 1; i <= N; i += world_size)
-        sum += f(a + h * (i - 0.5));
-    return 4 * h * sum;
+	double h, sum = 0;
+	int a = 0, b = 1;
+	h = 1.0 / N;
+	for (int i = world_rank + 1; i <= N; i += world_size)
+		sum += f(a + h * (i - 0.5));
+	return 4 * h * sum;
 }
 
 double trapezoidal_method(int N, int world_size, int world_rank) 
 {
-    double h, sum = 0;
-    int a = 0, b = 1;
-    h = 1.0 / N;
-    for (int i = world_rank + 1; i < N; i += world_size)
-        sum += f(a + i * h);
-    return 4 * h * ((f(a) + f(b)) / 2 + sum);
+	double h, sum = 0;
+	int a = 0, b = 1;
+	h = 1.0 / N;
+	for (int i = world_rank + 1; i < N; i += world_size)
+		sum += f(a + i * h);
+	return 4 * h * ((f(a) + f(b)) / 2 + sum);
 }
 
 double simpson_method(int N, int world_size, int world_rank) 
 {
-    double h, sum = 0, summ = 0;
-    int a = 0, b = 1;
-    h = 1.0 / N;
-    for (int i = world_rank + 1; i < N; i += world_size)
-    {
-        sum += f(a + h * (i - 0.5));
-        summ += f(a + i * h);
-    }
-    sum += f(a + h * (N - 0.5));
-    return 4 * (h / 3) * ((f(a) + f(b)) / 2 + (2 * sum) + summ);
+	double h, sum = 0, summ = 0;
+	int a = 0, b = 1;
+	h = 1.0 / N;
+	for (int i = world_rank + 1; i < N; i += world_size)
+	{
+		sum += f(a + h * (i - 0.5));
+		summ += f(a + i * h);
+	}
+	sum += f(a + h * (N - 0.5));
+	return 4 * (h / 3) * ((f(a) + f(b)) / 2 + (2 * sum) + summ);
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    int N = (argc == 2) ? atoi(argv[1]) : DEFAULT_N;
+    int N = (argc > 1) ? atoi(argv[1]) : DEFAULT_N;
+    int method = (argc > 1) ? atoi(argv[2]) : DEFAULT_METHOD;
     double MyPi, term;
     clock_t StartClock, EndClock;
 
@@ -75,10 +83,13 @@ int main(int argc, char* argv[])
     // Broadcasts a message from the process with rank "root"
     // to all other processes of the communicator
     MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-    term = rectangle_method(N, world_size, world_rank);
-    //term = trapezoidal_method(N, world_size, world_rank);
-    //term = simpson_method(N, world_size, world_rank);
+    
+    if (method == 1)
+        term = rectangle_method(N, world_size, world_rank);
+    else if (method == 2)
+        term = trapezoidal_method(N, world_size, world_rank);
+    else
+        term = simpson_method(N, world_size, world_rank);
 
     // Reduces values on all processes to a single value
     MPI_Reduce(&term, &MyPi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
@@ -95,7 +106,7 @@ int main(int argc, char* argv[])
         TotalTime = (double)(EndClock - StartClock) / CLOCKS_PER_SEC;
         printf("\n Computing time = %f sec\n", TotalTime);
     }
-	
+
     MPI_Finalize();
     return 0;
-} 
+}
